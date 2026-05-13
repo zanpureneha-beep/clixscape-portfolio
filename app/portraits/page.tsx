943cea2b2ec6f60f3f118bda73819daeaa10270b
Neha Zanpure <zanpureneha@gmail.com>
2026-05-13 14:25:01 +0530

    removed broken portrait image block

--- app/portraits/page.tsx
+++ app/portraits/page.tsx
@@ -71,10 +71,10 @@ export default function PortraitsPage() {
 />
 
 <img
-  src="/images/fresh.jpg"
-  alt="Portrait 8"
-  className="h-[680px] w-full rounded-3xl object-cover"
-/>
+src="/images/fresh1.jpg"
+alt="Portrait 9"
+className="h-[680px] w-full rounded-3xl object-cover"
+/>  
 
 
 
